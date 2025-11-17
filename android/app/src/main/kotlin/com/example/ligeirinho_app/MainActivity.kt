c8ebffe68784fb4139f73322b988884c906d8747
Lucas Boeck <lucaskrobel1@gmail.com>
2025-11-17 10:44:36 -0300

    Commit inicial: aparição do mapa

---
+++ android/app/src/main/kotlin/com/example/ligeirinho_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ligeirinho_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        cameraConstraint: CameraConstraint.contain(
+          bounds: LatLngBounds.fromPoints([
+            const LatLng(-25.245742, -48.939075),
+            const LatLng(-25.676351, -49.435638),
+          ]),
+        ),
+        initialCenter: const LatLng(-25.442306210992452, -49.23861780095272),
+        initialZoom: 13.0,
+        minZoom: 12.0,
+        maxZoom: 16.0,
+      ),
+
+      children: [
+        TileLayer(
+          urlTemplate:
+              'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpeStreetMap contributors',
+              onTap: () =>
+                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

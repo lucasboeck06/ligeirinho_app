ee7aea400ff4b656563d333fb209d3f2f38c98c5
Lucas Boeck <lucaskrobel1@gmail.com>
2025-11-17 15:17:43 -0300

    Otimização dos quadros do mapa em 4x

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -25,20 +28,30 @@ class HomePage extends StatelessWidget {
       options: MapOptions(
         cameraConstraint: CameraConstraint.contain(
           bounds: LatLngBounds.fromPoints([
-            const LatLng(-25.245742, -48.939075),
-            const LatLng(-25.676351, -49.435638),
+            const LatLng(
+              -25.245742,
+              -48.939075,
+            ), // Coord superior/direira máxima
+            const LatLng(
+              -25.676351,
+              -49.435638,
+            ), // Coord iinferior/esquerda máxima
           ]),
         ),
-        initialCenter: const LatLng(-25.442306210992452, -49.23861780095272),
-        initialZoom: 13.0,
-        minZoom: 12.0,
-        maxZoom: 16.0,
+        initialCenter: const LatLng(
+          -25.43735519478566,
+          -49.27010334955912,
+        ), // Posição inicial, meio de Curitiba
+        initialZoom: 13.0, // Define o zoom inicial do App
+        minZoom: 12.0, // Valor mínimo (pode afastar até...)
+        maxZoom: 16.0, // Valor máximo (pode ampliar até...)
       ),
 
       children: [
         TileLayer(
           urlTemplate:
-              'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+              'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', // Puxa o png do mapa do CartoDB
+          retinaMode: true, // Define a resolução do mapa para alta-qualdiade
           userAgentPackageName: 'com.example.app',
         ),
         RichAttributionWidget(
